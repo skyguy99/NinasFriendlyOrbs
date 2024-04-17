88ea4bcd5514ba7ffbb3254c79df782fcf0846d0
Skylar Thomas <skytblue@gmail.com>
2024-04-17 00:37:38 -0700

    motions loop with countdowns

--- Assets/NinasFriendlyOrbs/Scripts/MotionsManager.cs
+++ Assets/NinasFriendlyOrbs/Scripts/MotionsManager.cs
@@ -9,6 +9,8 @@ public class MotionsManager : MonoBehaviour
     [SerializeField] private MotionStateManager motionStateManager;
     [SerializeField] public int currentMotion;
 
+    [SerializeField] public float trackerDefaultSpeed = 0.5f;
+    [SerializeField] private float trackerCurrentSpeed;
     [SerializeField] public int motionLoopCount = 3;
     private int currentMotionLoopCount;
     
@@ -25,10 +27,53 @@ public class MotionsManager : MonoBehaviour
             splineRight = r;
         }
     }
+    
+    //TODO: Output countdown with copy to a label
+    IEnumerator Countdown()
+    {
+        int count = 3;
+       
+        while (count > 0) {
+           
+            // display something...
+            yield return new WaitForSeconds(1);
+            count --;
+            Debug.Log("Start countdown: "+count);
+        }
+       
+        // Start
+        Debug.Log("== START GAME ==");
+        
+        motionStateManager.GetTrackingLeft().SetMotionSpeed(trackerDefaultSpeed);
+        motionStateManager.GetTrackingRight().SetMotionSpeed(trackerDefaultSpeed);
+    }
+    
+    //TODO: Output countdown with copy to a label
+    IEnumerator WaitToStartNextMotion()
+    {
+        int count = 3;
+        while (count > 0) {
+           
+            // display something...
+            yield return new WaitForSeconds(1);
+            count --;
+            Debug.Log("Next motion countdown: "+count);
+        }
+       
+        // Start
+        Debug.Log("== SWITCHING MOTION TO INDEX: "+currentMotion+" ==");
+        
+        motionStateManager.GetTrackingLeft().SetMotionSpeed(trackerDefaultSpeed);
+        motionStateManager.GetTrackingRight().SetMotionSpeed(trackerDefaultSpeed);
+        
+        motionStateManager.GetTrackingLeft().SwitchMotionSpline(motions[currentMotion].splineLeft.GetComponent<SplineComputer>());
+        motionStateManager.GetTrackingRight().SwitchMotionSpline(motions[currentMotion].splineRight.GetComponent<SplineComputer>());
+    }
 
     private void Start()
     {
         motionStateManager = GetComponent<MotionStateManager>();
+        StartCoroutine(Countdown());
     }
 
     public void MotionDidFinish() //called by spline on end
@@ -44,18 +89,19 @@ public class MotionsManager : MonoBehaviour
 
     private void SwitchMotion()
     {
+        motionStateManager.GetTrackingLeft().SetMotionSpeed(0f);
+        motionStateManager.GetTrackingRight().SetMotionSpeed(0f);
+        
         if (currentMotion < motions.Count-1)
             currentMotion++;
         else
             currentMotion = 0;
-        
-        Debug.Log("SWITCHING MOTION TO INDEX: "+currentMotion);
-        
-        motionStateManager.GetTrackingLeft().SwitchMotionSpline(motions[currentMotion].splineLeft.GetComponent<SplineComputer>());
-        motionStateManager.GetTrackingRight().SwitchMotionSpline(motions[currentMotion].splineRight.GetComponent<SplineComputer>());
+
+        StartCoroutine(WaitToStartNextMotion());
     }
     private void Update()
     {
-        
+        //motionStateManager.GetTrackingLeft().SetMotionSpeed(trackerCurrentSpeed);
+       // motionStateManager.GetTrackingRight().SetMotionSpeed(trackerCurrentSpeed);
     }
 }
